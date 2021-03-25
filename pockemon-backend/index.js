6020eec247fd40ac34343caa8cbfc7747120904d
camiaguado <camiaguado@hotmail.com>
2021-03-25 14:51:02 +0100

    conectando a la base de datos

--- pockemon-backend/index.js
+++ pockemon-backend/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // crear el servidor
 const app = express();
+
+// conectar a mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/pokemon', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
 
 // puerto y arrancar servidor
 app.listen(4000, () => {
